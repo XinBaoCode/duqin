a18da96137fe0f2246b40f7a27c1e32098f8815b
XinBaoCode <1392368311@qq.com>
2022-06-27 13:56:14 +0800

    底部导航一

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -7,11 +7,60 @@ class RootPage extends StatefulWidget {
   State<RootPage> createState() => _RootPageState();
 }
 
+const Map<String, String> _buttonNames = {
+  'home': '首页',
+  'music': '音乐',
+  'create_media': '',
+  'tiny_video': '小视频',
+  'profile': '我的',
+};
+
 class _RootPageState extends State<RootPage> {
+  // 当前选中页索引
+  int _currentIndex = 0;
+  // 底部
+  final List<BottomNavigationBarItem> _bottomNavBarList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _buttonNames.forEach((key, value) {
+      _bottomNavBarList.add(_bottomNavBarItem(key, value));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(child: Text('RootPage')),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _bottomNavBarList,
+        currentIndex: _currentIndex,
+        onTap: _onTabClick,
+      ),
     );
+  }
+
+  BottomNavigationBarItem _bottomNavBarItem(String key, String value) {
+    return BottomNavigationBarItem(
+      icon: Image.asset(
+        'assets/images/icons/$key.png',
+        width: 24,
+        height: 24,
+      ),
+      activeIcon: Image.asset(
+        'assets/images/icons/${key}_active.png',
+        width: 24,
+        height: 24,
+      ),
+      label: value,
+    );
+  }
+
+  // 底部切换
+  void _onTabClick(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
